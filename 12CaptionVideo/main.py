9f724dd7548d95d9a213d4a289b83086bc2a0faa
jewey <872294742@qq.com>
2019-02-19 23:54:49 +0800

    完善功能

--- 12CaptionVideo/main.py
+++ 12CaptionVideo/main.py
@@ -8,12 +8,17 @@ import math
 
 matplotlib.rc("font", family = "MicroSoft YaHei", weight = "bold")
 
-duration = 4
+duration = 3
 fig,ax = plt.subplots(figsize=(10, 10)
                       # ,facecolor='black'
                     )#设置
 #基础参数
-my_text = {'text':u'test','x':0,'y':0,'y_mov':0,'size':0,'rot':0,'c_rot':0}
+text_obj = [
+    {'text':u'hello你们好','x':1,'y':0.7,'y_mov':0,'size':0,'rot':0,'c_rot':0},
+    {'text':u'你好啊','x':1,'y':0.5,'y_mov':0,'size':0,'rot':0,'c_rot':0},
+    {'text':u'test','x':1,'y':0.3,'y_mov':0,'size':0,'rot':0,'c_rot':0},
+    {'text':u'hello','x':1,'y':0,'y_mov':0,'size':0,'rot':0,'c_rot':0}
+        ]
 t_inc = 0.03#增量参数,摆脱t的控制
 '''
 渲染每一个时间t下的页面
@@ -25,17 +30,22 @@ def make_frame(t):
     ax.set_ylim(0, 2.5)
     ax.set_xlim(0, 2.5)
 
-    FontStart(my_text,50)
+    FontStart(0,50)
     return mplfig_to_npimage(fig)#生成一帧
 
 '''
 字体变大
 max_size 要变最大的字号
 '''
-def FontStart(obj,max_size):
+def FontStart(index,max_size):
+    if index>len(text_obj)-1:
+        print("超过最大len")
+        return
+    obj = text_obj[index]
     f_size = min(obj['size'],max_size)
     if f_size==max_size:
-        YStart(obj=obj,y_max_move=0.2)
+        YStart(index,y_max_move=0.8)
+        FontStart(index+1,max_size)
         return
     else:
         f_size_inc = 150*t_inc
@@ -46,20 +56,21 @@ def FontStart(obj,max_size):
     size = obj['size']+f_size_inc
     #更新画布显示
     updateDraw(x,y,text,size,0)
-    setText(obj=obj,size=size)
+    setText(index,size=size)
 
 '''
 文字向上移动
 y_max_move 要向上移动的最大距离
 '''
-def YStart(obj,y_max_move):
+def YStart(index,y_max_move):
     print('YStart')
+    obj = text_obj[index]
     y_mov_tmp = min(obj['y_mov'],y_max_move)
     if y_mov_tmp == y_max_move:
-        RStart(obj,0,0,1)
+        RStart(index,1,0.5,1)
         return
     else:
-        y_mov_inc = 5*t_inc
+        y_mov_inc = 3*t_inc
     #设置属性
     text = obj['text']
     x = obj['x']
@@ -68,23 +79,24 @@ def YStart(obj,y_max_move):
     size = obj['size']
     #更新画布显示
     updateDraw(x,y,text,size,0)
-    setText(obj=obj,y=y,y_mov=y_mov)
+    setText(index,y=y,y_mov=y_mov)
 
 '''
 文字旋转90度
 dire 旋转方向，1为向左旋转90度，-1为向右旋转90度
 dot_x,dot_y 圆点坐标
 '''
-def RStart(obj,dot_x,dot_y,dire=1):
+def RStart(index,dot_x,dot_y,dire=1):
     print('RStart')
+    obj = text_obj[index]
     rot_ang = azimuthAngle(dot_x,dot_y,obj['x'],obj['y'])
     print('rot_ang:%f'%rot_ang)
     #如果第一次旋转，记录一下当前初始的偏差角度
     #这里可能存在一个bug，即非第一次旋转时，obj['rot']为0，由于只旋转两次，暂时应该不会出现这个bug
     if obj['rot'] == 0:
-        setText(obj,rot=rot_ang,c_rot=rot_ang)
+        setText(index,rot=rot_ang,c_rot=rot_ang)
     # setText(c_rot=rot_ang)
-    rot = obj['c_rot']+5*t_inc#旋转角度
+    rot = obj['c_rot']+110*t_inc#旋转角度
     # rot = rot_ang+5*t_inc#旋转角度
     r = get_two_points_len(dot_x,dot_y,obj['x'],obj['y'])#获取旋转半径
 
@@ -99,7 +111,7 @@ def RStart(obj,dot_x,dot_y,dire=1):
 
     #更新画布显示
     updateDraw(x,y,text,size,rot-obj['rot'])
-    setText(obj=obj,x=x,y=y,c_rot=rot_ang)
+    setText(index,x=x,y=y,c_rot=rot)
 '''
 更新画布
 '''
@@ -107,7 +119,8 @@ def updateDraw(x,y,text,size,rotation):
     plt.text(x, y,text ,horizontalalignment='left',verticalalignment='bottom',
                  fontdict = {'size': size, 'color': 'red','rotation':rotation})
 #保存文字属性
-def setText(obj,**kw):
+def setText(index,**kw):
+    obj = text_obj[index]
     if kw.has_key('x'):
         obj['x'] = kw['x']
     if kw.has_key('y'):
@@ -120,7 +133,7 @@ def setText(obj,**kw):
         obj['y_mov'] = kw['y_mov']
     if kw.has_key('c_rot'):
         obj['c_rot'] = kw['c_rot']
-    print(my_text)
+    print(obj)
 '''
 计算两点连线与x轴的方位角
 x1,  y1 起始点坐标
